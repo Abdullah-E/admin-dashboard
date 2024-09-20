24aa212cbfe646a4221b322180e6dcca0604cf50
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-09-21 03:46:22 +0500

    hosted backend

--- src/api/config.js
+++ src/api/config.js
@@ -1,1 +1,5 @@
-export const baseApi = "http://localhost:5000/api/v1";
+//for local:
+// export const baseApi = "http://localhost:5000/api/v1";
+
+//for remote:
+export const baseApi = "https://keibo-client-server.vercel.app/api/v1";
